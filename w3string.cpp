ecbbbfe71a4e272fae5a60a1fcfe242674cc23fb
Gulshan Gupta <70631843+GullyGupta@users.noreply.github.com>
2021-06-20 14:47:42 +0530

    Add files via upload

---
+++ w3string.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{ 
+string fullName;
+cout << "Type your full name: ";
+getline (cin, fullName);
+cout << "Your name is: " << fullName;
+return 0;
+}
+
+// Type your full name: John Doe
+// Your name is: John Doe

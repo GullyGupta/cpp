0d218cbc7c3829ed002d9684b25136216f673f61
Gulshan Gupta <70631843+GullyGupta@users.noreply.github.com>
2021-06-22 22:52:39 +0530

    Add files via upload

---
+++ do_while_choice.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int userchoice = 0;
+    do
+    {
+        cout << "Hello welcome to my menu \n";
+        cout << "Please enter your choice from this menu. \n";
+        cout << "1. continue\n";
+        cout << "2. quit\n";
+        cin >> userchoice;
+    }while( userchoice == 1);
+
+}
+   
